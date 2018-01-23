f30b2501bb9976acc0f5412d368044e998f3177d
LiJingss <m13681423827@163.com>
2018-01-23 13:48:35 +0800

    del

--- src/components/logo.vue
+++
@@ -1,5 +0,0 @@
-<template lang="html">
-	<div class="logo">
-		<router-link :to="{path:'/healthindex'}"><img src="../assets/img/nav.png"/></router-link>
-	</div>
-</template>
--- src/views/Health/healthinfo.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<div id="healthinfo">
-		healthinfo
-	</div>
-</template>
